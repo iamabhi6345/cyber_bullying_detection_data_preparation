def736c45105fea757f12206d19b5c03ac1bafe9
ABHISHEK KUMAR <iamabhi6345@gmail.com>
2024-07-04 20:19:19 +0530

    configuration added for data preparation

---
+++ abhishek/config_schemas/infrastructure/gcp_schema.py
@@ -0,0 +1,14 @@
+from hydra.core.config_store import ConfigStore
+from pydantic.dataclasses import dataclass
+
+
+@dataclass
+class GCPConfig:
+    project_id: str = "cyberbullying-428016"
+    zone: str = "asia-south1-a"
+    network: str = "default"
+
+
+def setup_config() -> None:
+    cs = ConfigStore.instance()
+    cs.store(name="gcp_config_schema", node=GCPConfig)
--- abhishek/process_data.py
+++ abhishek/process_data.py
@@ -1,26 +1,19 @@
-from abhishek.config_schemas.config_schema import Config
+from abhishek.config_schemas.data_processing_config_schema import DataProcessingConfig
 from abhishek.utils.config_utils import get_config
 from abhishek.utils.gcp_utils import access_secret_version
 from abhishek.utils.data_utils import get_raw_data_with_version
 
 
-@get_config(config_path="../configs", config_name="config")
-def process_data(config: Config) -> None: 
-    version = "v1" 
-    data_local_save_dir = "./data/raw" 
-    dvc_remote_repo = "https://github.com/iamabhi6345/cyber_bullying_detection_dvc.git" 
-    dvc_data_folder = "data/raw" 
-    github_user_name = "iamabhi6345" 
-    # github_access_token = access_secret_version("iamabhi6345", "abhishek-data-github-access-token") 
-    github_access_token = "SECRET TOKENS DONOT COMMIT" 
-
+@get_config(config_path="../configs", config_name="data_processing_config")
+def process_data(config: DataProcessingConfig) -> None: 
+   
     get_raw_data_with_version(
-    version=version,
-    data_local_save_dir=data_local_save_dir,
-    dvc_remote_repo=dvc_remote_repo,
-    dvc_data_folder=dvc_data_folder,
-    github_user_name=github_user_name,
-    github_access_token=github_access_token
+    version=config.version,
+    data_local_save_dir=config.data_local_save_dir,
+    dvc_remote_repo=config.dvc_remote_repo,
+    dvc_data_folder=config.dvc_data_folder,
+    github_user_name=config.github_user_name,
+    github_access_token="YOUR OWN GITHUB ACCESS TOKEN"
     )
 
 
