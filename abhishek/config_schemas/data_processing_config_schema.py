d8bb516924667e05e1839c80109e5f888028467f
abhishek <iamabhi6345@gmail.com>
2024-07-05 16:28:31 +0530

    dataset cleaner schema added

--- abhishek/config_schemas/data_processing_config_schema.py
+++ abhishek/config_schemas/data_processing_config_schema.py
@@ -2,7 +2,7 @@ from hydra.core.config_store import ConfigStore
 from omegaconf import MISSING
 from pydantic.dataclasses import dataclass
 from abhishek.config_schemas.infrastructure.gcp_schema import GCPConfig
-from abhishek.config_schemas.data_processing import dataset_readers_schema
+from abhishek.config_schemas.data_processing import dataset_readers_schema ,dataset_cleaners_schema
 from abhishek.config_schemas.infrastructure import gcp_schema
 
 @dataclass
@@ -16,6 +16,7 @@ class DataProcessingConfig:
 
     infrastructure : gcp_schema.GCPConfig = gcp_schema.GCPConfig()
     dataset_reader_manager: dataset_readers_schema.DatasetReaderManagerConfig = MISSING
+    dataset_cleaner_manager: dataset_cleaners_schema.DatasetCleanerManagerConfig = MISSING
     
     
 
@@ -24,6 +25,7 @@ class DataProcessingConfig:
 def setup_config() -> None:
     gcp_schema.setup_config()
     dataset_readers_schema.setup_config()
+    dataset_cleaners_schema.setup_config()
     cs = ConfigStore.instance()
     cs.store(name="data_processing_config_schema", node=DataProcessingConfig)
     
